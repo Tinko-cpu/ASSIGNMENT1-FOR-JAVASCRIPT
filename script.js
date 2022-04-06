533c1d56072b643b6a533f1d5df8ace68c0690d5
Tinko-cpu <oseninurudeenafolabi@yahoo.com>
2022-04-06 13:33:44 +0100

    Create script.js

---
+++ script.js
@@ -0,0 +1,25 @@
+// 1) Variables are identifier or name space in computer memories Example: Dogs can be giving Name in order to diffenreciate them
+
+// 2) Data types in javascript . We have 7 data types in javascript 
+        // a) Strings: can be a character,letter which must be in single qoute or double qoute
+        //  examples: let myName = "Angela" or let myCharacter = "A"
+
+        // b) Numbers: Numbers in javascript can be any numbers, floats, integer
+            //  exmples: let firstNumber = 8; or let secondNumber = 78.56;
+        
+        // c) Boolean: This Data type can be either on or off , which represent with true or false or yes or no;
+            //  examples: let isGame = true;
+        
+        // d) Undefine
+
+
+// 3) ADDITION OF TWO NUMBERS
+
+let firstNumber = 20;
+let secondNumber = 12;
+let result = firstNumber + secondNumber;
+console.log(result);
+
+
+let x = 12;
+let myName = "John Doe";
